697ac682e14f8aa71536bfee5ab32b525a4c01ce
Anup Jeejo <jeejoanup@gmail.com>
2026-02-28 17:53:28 +0530

    Landing, Login, Register, forgot, reset, career-guide and resume-analyser page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import CareerGuide from "@/components/career-guide";
+import Hero from "@/components/hero";
+import Loading from "@/components/loading";
+import ResumeAnalyzer from "@/components/resume-analyser";
+import { useAppData } from "@/context/AppContext";
+
+const Home = () => {
+  const { loading } = useAppData();
+  if (loading) return <Loading />;
+  return (
+    <div>
+      <Hero />
+      <CareerGuide />
+      <ResumeAnalyzer/>
+    </div>
+  );
+};
+
+export default Home;
